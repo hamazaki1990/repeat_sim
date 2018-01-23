c23c23c954c44ab3ade33f346d66c5fcb56ef866
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2018-01-23 22:03:00 +0900

    create diploid

--- monomer.py
+++ monomer.py
@@ -1,9 +1,10 @@
+from decimal import Decimal
 import random
 import copy
 
 
 class Monomer:
-    mutationrate = 0.001
+    mutationrate = 0.01
 
     def __init__(self, n, f=1.0):
         self._id = n
@@ -19,14 +20,37 @@ class Monomer:
     def get_genotype(self):
         return self._genotype
 
-    def acquire_mutation(self):
+    def acquire_mutation(self, s=0.0):
         r = random.random()
         next_ind = copy.deepcopy(self)
         next_genotype = next_ind._genotype
+        next_fitness = next_ind._fitness
         if r < Monomer.mutationrate:
             next_genotype.append(random.random())
+            next_fitness = next_fitness + s
         next_ind._genotype = next_genotype
+        next_ind._fitness = next_fitness
         return next_ind
 
     def __repr__(self):
         return str(self._id)
+
+
+def main():
+    test = Monomer(1)
+    print(test.get_fitness())
+    test2 = test.acquire_mutation()
+    print(test2.get_id())
+    print(test2.get_genotype())
+    print(test2.get_fitness())
+    test3 = test.acquire_mutation(0.2)
+    print(test3.get_id())
+    print(test3.get_genotype())
+    print(test3.get_fitness())
+    test4 = test3.acquire_mutation(-0.1)
+    print(test4.get_id())
+    print(test4.get_genotype())
+    print(test4.get_fitness())
+
+if __name__ == '__main__':
+    main()
--- tandem_repeat.py
+++ tandem_repeat.py
@@ -1,6 +1,29 @@
-from monomer import monomer
+from monomer import Monomer
 
 
 class Tandem_repeat:
     def __init__(self, n, i=None, s=0.0):
-        inds = [Monomer(x) for x in range(n)]
+        repeats = [Monomer(x) for x in range(n)]
+        if i is not None:
+            repeats[i] = Monomer(i, 1+s)
+        self._repeats = repeats
+
+    def get_ids(self):
+        return self._repeats
+
+    def get_fitnesses(self):
+        fitness = [x.get_fitness() for x in self._repeats]
+        return fitness
+
+
+def main():
+    repeat = Tandem_repeat(5)
+    print(repeat.get_ids())
+    print(repeat.get_fitnesses())
+    repeat2 = Tandem_repeat(6, 1, 0.2)
+    print(repeat2.get_ids())
+    print(repeat2.get_fitnesses())
+
+
+if __name__ == '__main__':
+    main()
