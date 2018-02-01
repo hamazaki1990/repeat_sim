4db683ef8467fee9ec55d3b0efc9d2c9c9da71ad
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2018-02-01 19:34:29 +0900

    modify

--- diploid.py
+++ diploid.py
@@ -2,7 +2,10 @@ from Tandem_repeat import Tandem_repeat
 
 
 class Diploid:
+    conversionrate = 0.01
+
     def __init__(self, l1, l2, i1=None, s1=0.0):
         self._diploid = [Tandem_repeat(l1, i1, s1), Tandem_repeat(l2)]
 
-    def 
+    def gene_conversion(self):
+        
