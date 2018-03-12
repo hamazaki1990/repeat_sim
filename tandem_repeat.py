b00fa649cd68029c24dee8f37a8b7a0beae3ce74
hamazaki1990 <midsummerbeach@yahoo.co.jp>
2018-03-12 13:25:39 +0900

    haploid tandem repeat

--- tandem_repeat.py
+++ tandem_repeat.py
@@ -4,9 +4,9 @@ from monomer import Monomer
 
 
 class Tandem_repeat:
-    mutationrate = 1
-    conversionrate = 1
-    slippagerate = 1
+    mutationrate = 0.01
+    conversionrate = 0.01
+    slippagerate = 0.01
 
     def __init__(self, l, i=None, s=0.0):
         repeats = [Monomer(x) for x in range(l)]
@@ -68,27 +68,32 @@ def main():
     print(repeat.get_fitnesses())
     print(repeat.get_length())
     print(repeat.get_genotypes())
+    print(repeat.cal_fitness())
     repeat2 = repeat.acquire_mutation(0.1)
     print(repeat2.get_ids())
     print(repeat2.get_fitnesses())
     print(repeat2.get_genotypes())
+    print(repeat2.cal_fitness())
     repeat3 = repeat2.acquire_mutation(0.2)
     print(repeat3.get_ids())
     print(repeat3.get_fitnesses())
     print(repeat3.get_genotypes())
+    print(repeat3.cal_fitness())
     repeat4 = repeat3.slippage()
     print(repeat4.get_ids())
     print(repeat4.get_fitnesses())
     print(repeat4.get_genotypes())
+    print(repeat4.cal_fitness())
     repeat5 = repeat3.gene_conversion()
     print(repeat5.get_ids())
     print(repeat5.get_fitnesses())
     print(repeat5.get_genotypes())
+    print(repeat5.cal_fitness())
     repeat6 = repeat5.gene_conversion()
     print(repeat6.get_ids())
     print(repeat6.get_fitnesses())
     print(repeat6.get_genotypes())
-
+    print(repeat6.cal_fitness())
 
 if __name__ == '__main__':
     main()
