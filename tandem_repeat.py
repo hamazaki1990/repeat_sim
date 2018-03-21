0446b5bf8f45cc07eeba247dd1f6cb88748e78c3
hamazaki <hamazaki@nemurinezumi.local>
2018-03-21 23:26:06 +0900

    modify

--- tandem_repeat.py
+++ tandem_repeat.py
@@ -8,8 +8,8 @@ class Tandem_repeat:
     conversionrate = 1
     slippagerate = 1
 
-    def __init__(self, l, i=None, s=0.0):
-        repeats = [Monomer(x) for x in range(l)]
+    def __init__(self, length, i=None, s=0.0):
+        repeats = [Monomer(x) for x in range(length)]
         if i is not None:
             repeats[i] = Monomer(i, 1+s)
         self.__repeats = repeats
@@ -28,10 +28,13 @@ class Tandem_repeat:
         genotype = [x.get_genotype() for x in self.__repeats]
         return genotype
 
-    def cal_fitness(self):
+    def calculate_fitness(self):
         l = len(self.__repeats)
         f = [x.get_fitness() for x in self.__repeats]
         return sum(f)/l
+
+    def copy_self(self):
+        return copy.deepcopy(self)
 
     def acquire_mutation(self, s=0.0):  # add mutation that raises fitness by s
         r = random.random()
@@ -62,32 +65,32 @@ def main():
     print(repeat.get_fitnesses())
     print(repeat.get_length())
     print(repeat.get_genotypes())
-    print(repeat.cal_fitness())
+    print(repeat.calculate_fitness())
     repeat2 = repeat.acquire_mutation(0.1)
     print(repeat2.get_ids())
     print(repeat2.get_fitnesses())
     print(repeat2.get_genotypes())
-    print(repeat2.cal_fitness())
+    print(repeat2.calculate_fitness())
     repeat3 = repeat2.acquire_mutation(0.2)
     print(repeat3.get_ids())
     print(repeat3.get_fitnesses())
     print(repeat3.get_genotypes())
-    print(repeat3.cal_fitness())
+    print(repeat3.calculate_fitness())
     repeat4 = repeat3.slippage()
     print(repeat4.get_ids())
     print(repeat4.get_fitnesses())
     print(repeat4.get_genotypes())
-    print(repeat4.cal_fitness())
+    print(repeat4.calculate_fitness())
     repeat5 = repeat3.gene_conversion()
     print(repeat5.get_ids())
     print(repeat5.get_fitnesses())
     print(repeat5.get_genotypes())
-    print(repeat5.cal_fitness())
+    print(repeat5.calculate_fitness())
     repeat6 = repeat5.gene_conversion()
     print(repeat6.get_ids())
     print(repeat6.get_fitnesses())
     print(repeat6.get_genotypes())
-    print(repeat6.cal_fitness())
+    print(repeat6.calculate_fitness())
 
 
 if __name__ == '__main__':
