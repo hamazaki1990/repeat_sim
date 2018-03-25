0f87b22b4b97b7ab7e0a6e43d23e39ca3224831a
hamazaki <hamazaki@nemurinezumi.local>
2018-03-25 22:46:48 +0900

    diploid

--- diploid.py
+++ diploid.py
@@ -4,46 +4,49 @@ import copy
 
 
 class Diploid:
-    mutationrate = 1
-    conversionrate = 1
-    crossingrate = 1
-    slippagerate = 1
+    mutationrate = 0.25
+    sliprate = 0.25
+    conversrate = 0.25
+    crossingrate = 0.25
 
-    def __init__(self, l, i=None, s=0.0):
-        self._paternal = Tandem_repeat(l, i, s)
-        self._maternal = Tandem_repeat(l)
+    def __init__(self, ind_id, len1, len2, i=None, s=0.0):
+        self._paternal = Tandem_repeat(ind_id, len1, i, s)
+        self._maternal = Tandem_repeat(ind_id, len2)
 
-    def get_diploid_ids(self):
-        ids = [self._paternal.get_repeat_ids(),
-               self._maternal.get_repeat_ids()]
+    def get_ind_id(self):
+        return self._paternal.get_repeat_id()
+
+    def get_ind_repeatids(self):
+        ids = [self._paternal.get_monomer_ids(),
+               self._maternal.get_monomer_ids()]
         return ids
 
-    def get_diploid_lengths(self):
+    def get_ind_repeatlengths(self):
         length = [self._paternal.get_repeat_length(),
                   self._maternal.get_repeat_length()]
         return length
 
-    def get_diploid_fitnesses(self):
+    def get_ind_fitnesses(self):
         fitnesses = [self._paternal.get_repeat_fitnesses(),
                      self._maternal.get_repeat_fitnesses()]
         return fitnesses
 
-    def get_diploid_genotypes(self):
+    def get_ind_genotypes(self):
         genotype = [self._paternal.get_repeat_genotypes(),
                     self._maternal.get_repeat_genotypes()]
         return genotype
 
-    def cal_diploid_fitnesses(self):
+    def calculate_ind_fitnesses(self):
         dom = max(self._paternal.cal_repeat_fitness(),
-                  self._maternal.cal_repeat_fitness)
+                  self._maternal.cal_repeat_fitness())
         res = min(self._paternal.cal_repeat_fitness(),
-                  self._maternal.cal_repeat_fitness)
+                  self._maternal.cal_repeat_fitness())
         return sum(f)/l
 
     def acquire_mutation(self, s=0.0):  # add mutation that raises fitness by s
         r = random.random()
         next_repeats = copy.deepcopy(self)
-        if r < Tandem_repeat.mutationrate:
+        if r < 0.5:
             i = random.randrange(len(next_repeats._repeats))
             next_monomer = next_repeats._repeats[i]
             next_monomer._fitness += s
@@ -71,8 +74,23 @@ class Diploid:
     def crossing_over(self):
         r = random.random()
         next_repeats = copy.deepcopy(self)
-        if r < Tandem_repeat.crossingraterate:
 
+
+    def replicate_error(self):
+        error = [Tandem_repeat.mutationrate, Tandem_repeat.sliprate,
+                 Tandem_repeat.conversrate, crossingrate]
+        errorrate = [sum(error[:i]) for i in range(1, len(error) + 1)]
+        r = random.random()
+        if r < errorrate[0]:
+            return self.acquire_mutation()
+        elif r < errorrate[1]:
+            return self.slippage()
+        elif r < errorrate[2]:
+            return self.gene_conversion()
+        elif r < errorrate[3]:
+            return self.crossing_over()
+        else:
+            return self
 
 
 test = Diploid(3)
--- tandem_repeat.py
+++ tandem_repeat.py
@@ -1,67 +1,98 @@
+import random
 from monomer import Monomer
 
 
 class Tandem_repeat:
 
-    def __init__(self, l, i=None, s=0.0):
-        repeats = [Monomer(x) for x in range(l)]
-        if i is not None:
-            repeats[i] = Monomer(i, 1+s)
-        self._repeats = repeats
+    def __init__(self, repeat_id, length, mutatemonomer=None, s=0.0):
+        repeats = [Monomer(x) for x in range(length)]
+        if mutatemonomer is not None:
+            repeats[mutatemonomer].acquire_mutation(s)
+        self.__repeats = repeats
+        self.__repeat_id = repeat_id
 
-    def get_repeat_ids(self):
-        return self._repeats
+    def get_repeat_id(self):
+        return self.__repeat_id
 
-    def get_repeat_length(self):
-        return len(self._repeats)
+    def get_monomer_ids(self):
+        return [x.get_id() for x in self.__repeats]
 
-    def get_repeat_fitnesses(self):
-        fitness = [x.get_fitness() for x in self._repeats]
+    def get_length(self):
+        return len(self.__repeats)
+
+    def get_fitnesses(self):
+        fitness = [x.get_fitness() for x in self.__repeats]
         return fitness
 
-    def get_repeat_genotypes(self):
-        genotype = [x.get_genotype() for x in self._repeats]
+    def get_genotypes(self):
+        genotype = [x.get_genotype() for x in self.__repeats]
         return genotype
 
-    def cal_repeat_fitness(self):
-        l = len(self._repeats)
-        f = [x.get_fitness() for x in self._repeats]
+    def calculate_fitness(self):
+        l = len(self.__repeats)
+        f = [x.get_fitness() for x in self.__repeats]
         return sum(f)/l
+
+    def acquire_mutation(self, s=0.0):  # add mutation that raises fitness by s
+        i = random.randrange(len(self.__repeats))
+        self.__repeats[i] = self.__repeats[i].copy_self().acquire_mutation(s)
+        return self
+
+    def slippage(self):
+        if 1 < self.get_length():
+            i = random.randrange(self.get_length())
+            self.__repeats.pop(i)
+        return self
+
+    def gene_conversion(self):
+        i = random.randrange(self.get_length())
+        j = random.randrange(self.get_length())
+        self.__repeats[i] = self.__repeats[j]
+        return self
+
 
 
 
 def main():
-    repeat = Tandem_repeat(5)
+    repeat = Tandem_repeat(5, 1)
     print(repeat.get_ids())
     print(repeat.get_fitnesses())
     print(repeat.get_length())
     print(repeat.get_genotypes())
-    print(repeat.cal_fitness())
+    print(repeat.calculate_fitness())
     repeat2 = repeat.acquire_mutation(0.1)
     print(repeat2.get_ids())
     print(repeat2.get_fitnesses())
     print(repeat2.get_genotypes())
-    print(repeat2.cal_fitness())
+    print(repeat2.calculate_fitness())
     repeat3 = repeat2.acquire_mutation(0.2)
     print(repeat3.get_ids())
     print(repeat3.get_fitnesses())
     print(repeat3.get_genotypes())
-    print(repeat3.cal_fitness())
+    print(repeat3.calculate_fitness())
     repeat4 = repeat3.slippage()
     print(repeat4.get_ids())
     print(repeat4.get_fitnesses())
     print(repeat4.get_genotypes())
-    print(repeat4.cal_fitness())
+    print(repeat4.calculate_fitness())
     repeat5 = repeat3.gene_conversion()
     print(repeat5.get_ids())
     print(repeat5.get_fitnesses())
     print(repeat5.get_genotypes())
-    print(repeat5.cal_fitness())
+    print(repeat5.calculate_fitness())
     repeat6 = repeat5.gene_conversion()
     print(repeat6.get_ids())
     print(repeat6.get_fitnesses())
     print(repeat6.get_genotypes())
-    print(repeat6.cal_fitness())
+    print(repeat6.calculate_fitness())
+    repeat = Tandem_repeat(6)
+    print(repeat.get_ids())
+    print(repeat.get_genotypes())
+    for x in range(20):
+        repeat.replicate_error()
+        print(repeat.get_ids())
+        print(repeat.get_genotypes())
+
 
 if __name__ == '__main__':
     main()
